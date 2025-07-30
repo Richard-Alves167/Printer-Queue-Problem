7e78f7bc62f1d64dae90da0b2f1d00bfb628175e
Richard-Alves167 <riichardd.08@gmail.com>
2025-07-29 21:49:02 -0300

    FEAT: criação do comando para deletar os arquivos da fila de impressão.

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
+import os
 
+def ReloadPrintQueuePrinter():
+    os.system('net stop spooler"')
+    os.system('del /Q /F /S "%systemroot%\System32\spool\PRINTERS\*.*"')
+    os.system('net start spooler"')
+
+ReloadPrintQueuePrinter()
